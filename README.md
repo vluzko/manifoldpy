0b55d76dba81b51e3e9ff6c0ca915846dc1f6770
Vincent Luczkow <vincent.luczkow@gmail.com>
2022-07-28 19:27:44 -0700

    Setup for the post endpoints

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Tools for analyzing [Manifold Markets](https://manifold.markets/home) data.
 
 Currently has bindings for their API, and code for computing various accuracy metrics (Brier score, log score, calibration).
 
+
+## Calibration and Accuracy
--- manifold/calibration.py
+++ manifold/calibration.py
@@ -1,11 +1,13 @@
 import numpy as np
-from manifold.api import Market
+from manifold.api import Market, BinaryMarket
 from matplotlib import pyplot as plt
 from scipy.stats import beta
-from typing import List
+from typing import List, Tuple
 
 
-def extract_binary_probabilities(markets: List[Market]) -> np.ndarray:
+def extract_binary_probabilities(
+    markets: List[BinaryMarket],
+) -> Tuple[np.ndarray, np.ndarray]:
     """Get the closing probabilities from all binary markets.
     Markets that resolve NO have their probabilities flipped
     """
@@ -19,7 +21,7 @@ def brier_score(yes_probs: np.ndarray, no_probs: np.ndarray) -> float:
     Brier score is 1/n * sum((outcome - prob)^2) where outcome is 0 or 1.
     """
     yes_scores = (1 - yes_probs) ** 2
-    no_scores = no_probs ** 2
+    no_scores = no_probs**2
 
     num_mkts = len(yes_probs) + len(no_probs)
     score = 1 / num_mkts * (np.sum(yes_scores) + np.sum(no_scores))
@@ -42,9 +44,13 @@ def relative_log_score(markets: List[Market]) -> float:
     raise NotImplementedError
 
 
-def bet_counts(yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int) -> np.ndarray:
+def bet_counts(
+    yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int
+) -> np.ndarray:
     """Number of bets that have resolved YES/NO within each bins"""
-    yes_idx, yes_counts = np.unique(yes_probs.round(decimals=decimals), return_counts=True)
+    yes_idx, yes_counts = np.unique(
+        yes_probs.round(decimals=decimals), return_counts=True
+    )
     no_idx, no_counts = np.unique(no_probs.round(decimals=decimals), return_counts=True)
 
     all_vals = np.zeros((10**decimals + 1, 2))
@@ -53,7 +59,9 @@ def bet_counts(yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int) -> np
     return all_vals
 
 
-def binary_calibration(yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int=1) -> np.ndarray:
+def binary_calibration(
+    yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int = 1
+) -> np.ndarray:
     """Calculate binary calibration across all passed markets"""
     all_vals = bet_counts(yes_probs, no_probs, decimals)
 
@@ -62,8 +70,16 @@ def binary_calibration(yes_probs: np.ndarray, no_probs: np.ndarray, decimals: in
     return calibration
 
 
-def beta_binomial_calibration(yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int=1) -> np.ndarray:
+def beta_binomial_calibration(
+    yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int = 1
+) -> Tuple[np.ndarray, np.ndarray]:
     """Calculate calibration with a beta-binomial model
+
+    Args:
+        yes_probs:
+        no_probs:
+        decimals:
+
     Returns
         0.95 confidence interval and means for the distributions.
         Intervals are centered around the median.
@@ -81,27 +97,27 @@ def beta_binomial_calibration(yes_probs: np.ndarray, no_probs: np.ndarray, decim
 def plot_beta_binomial(upper_lower: np.ndarray, means: np.ndarray, decimals):
     _, ax = plt.subplots()
     num_bins = 10**decimals
-    x_axis = np.arange(0, 1+1/num_bins, 1/num_bins)
+    x_axis = np.arange(0, 1 + 1 / num_bins, 1 / num_bins)
     ax.scatter(x_axis, means, color="blue")
     ax.scatter(x_axis, upper_lower[:, 0], color="black", marker="_")
     ax.scatter(x_axis, upper_lower[:, 1], color="black", marker="_")
     plt.vlines(x_axis, upper_lower[:, 0], upper_lower[:, 1], color="black")
 
-    ax.set_xticks(np.arange(0, 1+1/10, 1/10))
+    ax.set_xticks(np.arange(0, 1 + 1 / 10, 1 / 10))
     ax.set_xlabel("Market probability")
-    ax.set_yticks(np.arange(0, 1 + 1/10, 1/10))
-    ax.set_ylabel('Beta binomial means and 0.95 intervals')
+    ax.set_yticks(np.arange(0, 1 + 1 / 10, 1 / 10))
+    ax.set_ylabel("Beta binomial means and 0.95 intervals")
 
     l = np.arange(0, x_axis.max(), 0.0001)
     ax.scatter(l, l, color="green", s=0.01, label="Perfect calibration")
     plt.show()
 
 
-def overall_calibration(yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int=1) -> np.ndarray:
-    scoring_rules = {'Brier': brier_score, 'Log': log_score}
+def overall_calibration(yes_probs: np.ndarray, no_probs: np.ndarray, decimals: int = 1):
+    scoring_rules = {"Brier": brier_score, "Log": log_score}
     scores = {k: v(yes_probs, no_probs) for k, v in scoring_rules.items()}
     print("\n".join(f"{k}: {v}" for k, v in scores.items()))
-    
+
     # Calibration with 100 bins
     one_percent = binary_calibration(yes_probs, no_probs, decimals=2)
     # plot_calibration(one_percent, bins=np.arange(0, 1+ 1/100, 1/100))
@@ -111,5 +127,7 @@ def overall_calibration(yes_probs: np.ndarray, no_probs: np.ndarray, decimals: i
     # plot_calibration(ten_percent, bins=np.arange(0, 1 + 1/10, 1/10))
 
     # Calibration when we model each bin as with a beta binomial model
-    beta_interval, beta_means = beta_binomial_calibration(yes_probs, no_probs, decimals=1)
+    beta_interval, beta_means = beta_binomial_calibration(
+        yes_probs, no_probs, decimals=1
+    )
     plot_beta_binomial(beta_interval, beta_means, decimals=1)
