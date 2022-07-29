b1f7a5be87ff55dcf1b04c20fe7cf7952a8ecf3c
Vincent Luczkow <vluzko@users.noreply.github.com>
2022-07-28 22:00:33 -0400

    Api update (#19)
    * API tests passing again
    
    * Added user, user_id, and slug API endpoints. Updated full_markets and all_markets to work with new API
    
    * users endpoint
    
    * Bets endpoint
    
    * All get endpoints implemented

--- tests/calibration_test.py
+++ tests/calibration_test.py
@@ -10,11 +10,11 @@ class MockMarket:
 
 
 def test_brier_score():
-    mkt = MockMarket(0.7, 'YES')
+    mkt = MockMarket(0.7, "YES")
     brier1 = calibration.brier_score([mkt])
     assert np.isclose(brier1, 0.09)
 
-    brier2 = calibration.brier_score([MockMarket(0.7, 'NO')])
+    brier2 = calibration.brier_score([MockMarket(0.7, "NO")])
     assert np.isclose(brier2, 0.49)
 
 
@@ -24,12 +24,12 @@ def test_log_score():
 
 def test_calibration():
     mkts = [
-        MockMarket(0.7, 'YES'),
-        MockMarket(0.7, 'YES'),
-        MockMarket(0.7, 'NO'),
+        MockMarket(0.7, "YES"),
+        MockMarket(0.7, "YES"),
+        MockMarket(0.7, "NO"),
     ]
     c = calibration.binary_calibration(mkts)
-    assert np.isclose(c[7], 2/3)
+    assert np.isclose(c[7], 2 / 3)
 
 
 def test_beta_binomial_calibration():
